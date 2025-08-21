b8fa2e451edb444276a31290c9c5b5dba5980ca7
Saeed Ghorbani <saeedgh@amazon.com>
2025-08-21 17:01:32 -0400

    update

--- assets/js/palette.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  const palettes = [null, 'palette-purple', 'palette-green', 'palette-red'];
-  const choice = palettes[Math.floor(Math.random() * palettes.length)];
-  if (choice) {
-    document.documentElement.classList.add(choice);
-  }
-})();
-
